804247f1b0c48b03518fcc66d8f7ef1a5f8b2950
Robert Karanja <robahndech@gmail.com>
2019-02-17 08:21:25 +0300

    The first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,129 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">First Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+<!-- added -->
+                         <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Last Name</label>
+
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control" name="last_name" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">User Name</label>
+
+                            <div class="col-md-6">
+                                <input id="user_name" type="text" class="form-control" name="user_name" required>
+                            </div>
+                        </div>
+<!-- ends here -->
+
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+<!-- added 
+                                <div class="form-group col-md-6">
+                                    <label for="company-name">Name<span class="required">*</span></label>
+
+                                    <input placeholder="Enter Name"
+                                            id="company-name"
+                                            required
+                                            name="name"
+                                            spellcheck="false"
+                                            class="form-control"
+                                            
+                                            />
+
+                                    </div>
+                                    
+
+<!-- ends here -->
+
+<!-- added 
+
+                                    <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+<!-- ends here -->
+
+
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/new/bootstrap.min.css">
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+            #welcome{
+                margin-top: 20px;
+            }
+            h1{
+                padding-top: 50px;
+                text-align: center;
+                color: black;
+            }
+            p{
+                text-align: center;
+                color: white;
+                font-size: 20px;
+            }
+            .list-group{
+                width: 100px;
+                align-items: center;
+                background-color: grey;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="well">
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                        
+                        @else
+                            <a href="{{ route('login') }}" ><button  class="btn btn-default">Login</button></a>
+                            <a href="{{ route('register') }}"><button  class="btn btn-default">Register</button></a>
+                        @endauth
+                    </div>
+                @endif
+
+       
+       <div class="well">
+                <h1><strong>Welcome to VetFarm</strong></h1> 
+            
+                <p style="color: limegreen;"><b><strong> <u> In this platform you have access to services like:<br>
+                </u></b></strong></p> 
+                <p style="color: white;"><b><strong>        
+                                Unlimited Access to Information Regarding Farming<br><br>
+                                Access to Veterinary Nearest your Location<br><br>
+                                Wide Market for Products<br><br>
+                               
+                </b></strong></p>
+                
+                <a  href="{{ url('/home') }}" class="btn btn-primary js-scroll-trigger btn btn-group-lg" style="margin-left: 600px"><strong>Get Started</strong></a> 
+        </div>
+            
+        </div>
+    </body>
+</html>
